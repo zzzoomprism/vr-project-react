ef86fe396aea94d6a95f29b55e1ef4e41c7e31fc
Zzzoomprism <s96k92@gmail.com>
2019-10-31 20:55:09 +0300

    Completed the last block.

--- src/components/HomeContent/AdvBlock/AdvBlock.jsx
+++ src/components/HomeContent/AdvBlock/AdvBlock.jsx
@@ -8,11 +8,12 @@ class AdvBlock extends React.Component{
             <div className={style.backgroundBlock}>
                 <img src="https://images.pexels.com/photos/775907/pexels-photo-775907.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
                     className={style.backgroundImg}  alt=""/>
-            </div>
-                 <div>
+                <div className={style.textUpToAnimImage}>
                     <h3>VISIT OUR VR_SITE!</h3>
-                     <a>VISITE!</a>
+                    <a href={'/vr'} target="_blank"><i className="fas fa-vr-cardboard"></i></a>
                 </div>
+            </div>
+
             </div>
         );
     }
