02cade1dd60dcb331b2fd96bc07de6a28ab947eb
zzzoomprism <s96k92@gmail.com>
2019-11-28 16:18:49 +0300

    created shop content page. Some animation

--- src/components/ShopContent/Content/Content.jsx
+++ src/components/ShopContent/Content/Content.jsx
@@ -6,55 +6,29 @@ import Button from "./../Button/Button";
 class Content extends React.Component{
 
     render() {
+        const furnitureInfo = info.map((el) => {
+            return  <div className={style.furnitureSection} key={el.id}>
+                <div className={style.funitureImageContent}>
+                    <img
+                        src={el.image}
+                        alt=""/>
+                    <div className={style.furniturePrice}>
+                        <h3>{el.price}</h3>
+                    </div>
+                </div>
+                <div className={style.funitureInformationContent}>
+                    <h3>{el.name}</h3>
+                    <p><span>{el.description}</span>
+                    </p>
+                    <div>
+                        <Button icon={"fas fa-shopping-cart"} btnId={el.id} content={"Add to card"}/>
+                        <Button icon={"fas fa-vr-cardboard"} btnId={el.id} content={"Look at VR"}/>
+                    </div>
+                </div>
+            </div>
+        });
         return <div className={style.mainFurnitureBlock}>
-            <div className={style.furnitureSection}>
-                <div className={style.funitureImageContent}>
-                    <img
-                        src="https://images.pexels.com/photos/276583/pexels-photo-276583.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                        alt=""/>
-                </div>
-                <div className={style.funitureInformationContent}>
-                    <h3>Title of furniture</h3>
-                    <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium debitis dicta in ipsa iste minima molestiae, nesciunt, officiis praesentium quaerat repellat soluta vel voluptatum? Facere hic itaque nulla obcaecati sapiente!</span>
-                    </p>
-                    <div>
-                        <Button icon={"fas fa-shopping-cart"} btnId={"1"} content={"Add to card"}/>
-                        <Button icon={"fas fa-vr-cardboard"} btnId={"1vr"} content={"Look at VR"}/>
-                    </div>
-                </div>
-            </div>
-            <div className={style.furnitureSection}>
-                <div className={style.funitureImageContent}>
-                    <img
-                        src="https://images.pexels.com/photos/3264493/pexels-photo-3264493.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                        alt=""/>
-                </div>
-                <div className={style.funitureInformationContent}>
-                    <h3>Title of furniture</h3>
-                    <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium debitis dicta in ipsa iste minima molestiae, nesciunt, officiis praesentium quaerat repellat soluta vel voluptatum? Facere hic itaque nulla obcaecati sapiente!</span>
-                    </p>
-                    <div>
-                        <Button icon={"fas fa-shopping-cart"} btnId={"1"} content={"Add to card"}/>
-                        <Button icon={"fas fa-vr-cardboard"} btnId={"1vr"} content={"Look at VR"}/>
-                    </div>
-                </div>
-            </div>
-            <div className={style.furnitureSection}>
-                <div className={style.funitureImageContent}>
-                    <img
-                        src="https://images.pexels.com/photos/584399/living-room-couch-interior-room-584399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                        alt=""/>
-                </div>
-                <div className={style.funitureInformationContent}>
-                    <h3>Title of furniture</h3>
-                    <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium debitis dicta in ipsa iste minima molestiae, nesciunt, officiis praesentium quaerat repellat soluta vel voluptatum? Facere hic itaque nulla obcaecati sapiente!</span>
-                    </p>
-                    <div>
-                        <Button icon={"fas fa-shopping-cart"} btnId={"1"} content={"Add to card"}/>
-                        <Button icon={"fas fa-vr-cardboard"} btnId={"1vr"} content={"Look at VR"}/>
-                    </div>
-                </div>
-            </div>
+            {furnitureInfo}
         </div>
     }
 }
--- src/components/ShopContent/TopMenu/TopMenu.jsx
+++ src/components/ShopContent/TopMenu/TopMenu.jsx
@@ -1,18 +1,37 @@
 import React from "react";
 import style from "./TopMenu.module.css";
 
-const TopMenu = (props) => {
-    return <div className={style.topMenuMainBlock}>
-        <div>
-            <h3>filter</h3>
+class TopMenu extends React.Component{
+    state = {
+        open: false,
+        height: "30px",
+    };
+    handleClickSortDown(){
+        if(window.screen.availWidth <= 640){
+            this.setState({open: true, height: "300px"})
+        }
+    }
+    render() {
+        return <div>
+            <div className={style.topMenuMainBlock}>
+                <div>
+                    <ul className={style.filterBlock} >
+                        <li onClick={this.handleClickSortDown.bind(this)}><i className="fas fa-sun"></i>Sort By:</li>
+                        <li><i className="fas fa-bolt"></i>top</li>
+                        <li><i className="fas fa-sort-amount-down"></i>price(exp)</li>
+                        <li><i className="fas fa-sort-amount-down-alt"></i>price(down)</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3><i className="fas fa-cart-plus"></i></h3>
+                </div>
+                <div>
+                    <h3><i className="fas fa-user-circle"></i></h3>
+                </div>
+            </div>
+            <hr/>
         </div>
-        <div>
-           <h3>cart</h3>
-        </div>
-        <div>
-            <h3>profile</h3>
-        </div>
-    </div>
+    }
 };
 
 export default TopMenu;
